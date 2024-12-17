6769d6325cd7c094f6e706d429e7add37fd37192
MatheusHrique <notch02005@gmail.com>
2024-12-16 21:47:19 -0300

    :bug: Fix: example.spec.js now working!

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -21,20 +21,47 @@ const { test, expect } = require("@playwright/test");
 
 test("tem título", async ({ page }) => {
   await page.goto("https://demo.playwright.dev/todomvc/#/");
-  await expect(page).toHaveTitle("todos");
+  await expect(page).toHaveTitle("React • TodoMVC");
 });
 
-test("verificar se adiciona uma nova linha", async ({ page }) => {
+test("verificar se está adicionando a lista corretamente", async ({ page }) => {
   await page.goto("https://demo.playwright.dev/todomvc/#/");
-
-  // Click the get started link.
-  // await page.getByRole('link', { name: 'Get started' }).click();
   await page.getByPlaceholder("What needs to be done?").fill("Linha 1");
   await page.keyboard.press("Enter");
-  await expect(page.locator(".todo-item")).toHaveText("Linha 1");
+  await expect(page.getByTestId("todo-title")).toHaveText(["Linha 1"]);
+});
 
-  // await expect(page.locator('ul > li')).toHaveText(['Text 1', 'Text 2', 'Text 3']);
+test("verificar se adiciona 3 linhas", async ({ page }) => {
+  await page.goto("https://demo.playwright.dev/todomvc/#/");
+  await expect(page.getByRole("listitem")).toHaveCount(0);
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 1");
+  await page.keyboard.press("Enter");
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 2");
+  await page.keyboard.press("Enter");
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 3");
+  await page.keyboard.press("Enter");
+  await expect(page.getByRole("listitem")).toHaveCount(6);
+});
 
-  // Expects page to have a heading with the name of Installation.
-  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("verificar se está indicando que falta 3 itens", async ({ page }) => {
+  await page.goto("https://demo.playwright.dev/todomvc/#/");
+  await expect(page.getByRole("listitem")).toHaveCount(0);
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 1");
+  await page.keyboard.press("Enter");
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 2");
+  await page.keyboard.press("Enter");
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 3");
+  await page.keyboard.press("Enter");
+  await expect(page.getByTestId("todo-count")).toHaveText("3 items left");
+});
+
+test("verificar se o botão de exclusão é clicado", async ({ page }) => {
+  await page.goto("https://demo.playwright.dev/todomvc/#/");
+  await page.getByPlaceholder("What needs to be done?").fill("Linha 1");
+  await page.keyboard.press("Enter");
+
+  const todoItems = page.getByTestId("todo-item");
+  await todoItems.nth(0).getByRole("checkbox").check();
+  await page.getByRole("button", { name: "Clear completed" }).click();
+  await expect(todoItems).toHaveCount(0);
 });
